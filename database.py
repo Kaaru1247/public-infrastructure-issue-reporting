82e79530840a183fd655aedc2909d3d58882d18c
Kaaru1247 <karunya1247@gmail.com>
2026-09-21 00:24:34 +0530

    Changed database to SQLite

--- database.py
+++ database.py
@@ -1,23 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-import os
-from urllib.parse import quote_plus
 
-load_dotenv()
+DATABASE_URL = "sqlite:///./infrastructure.db"
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{quote_plus(DB_PASSWORD)}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
-
-engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -26,3 +15,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
