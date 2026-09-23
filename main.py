f3df27f3f843a2f61842139f02ac708d67153cad
Kaaru1247 <karunya1247@gmail.com>
2026-09-23 22:59:42 +0530

    Allow deployed frontend in CORS

--- main.py
+++ main.py
@@ -59,9 +59,10 @@ app.mount(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://public-infrastructure-issue-reporting-1.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
