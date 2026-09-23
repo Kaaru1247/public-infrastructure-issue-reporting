40c50eb48f7020f8599a83ec5a3ba84e838a9ea7
Kaaru1247 <karunya1247@gmail.com>
2026-09-23 21:08:55 +0530

    Fix database table startup

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from database import engine, SessionLocal, Base
 # DATABASE
 # =========================================================
 
- Base.metadata.create_all(bind=engine)git add main.py
+Base.metadata.create_all(bind=engine)
 
 
 # =========================================================
